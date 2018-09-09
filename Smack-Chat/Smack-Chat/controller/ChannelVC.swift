adbbf5b8e7dd1fb402562a92278d5f064e05a4a2
MarwaZabara <marwa_zabara@yahoo.com>
2018-09-09 08:04:17 +0200

    Added menu button
    Added SWReveal menu button toggels ,also added tap and pan gesture recognizers

---
+++ Smack-Chat/Smack-Chat/controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Smack-Chat
+//
+//  Created by Marwa Zabara on 9/9/18.
+//  Copyright © 2018 Marwa Zabara. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+
+
+}
---
+++ Smack-Chat/Smack-Chat/controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Smack-Chat
+//
+//  Created by Marwa Zabara on 9/9/18.
+//  Copyright © 2018 Marwa Zabara. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var MenuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MenuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
+    }
+
+    
+
+  
+
+}
